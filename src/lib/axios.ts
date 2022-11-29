88d40e2e3d8824f46fff82070f65544dac64436d
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2022-11-29 23:24:27 +0900

    add:#7 envファイルの用意

---
+++ src/lib/axios.ts
@@ -0,0 +1,26 @@
+import Axios, { AxiosRequestConfig } from "axios";
+
+import { API_URL } from "../config";
+
+function authRequestInterceptor(config: AxiosRequestConfig) {
+  config.headers?.common?.setAccept("application/json");
+  return config;
+}
+/**
+ * ラッピングされたaxios関数
+ *
+ * こちらをHTTPクライアントはこちらを利用する
+ */
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.request.use(authRequestInterceptor);
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
