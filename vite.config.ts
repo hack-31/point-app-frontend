8d1805730d7e6ba6b50f811abaacb60ee5c3fd2e
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2022-11-29 23:23:17 +0900

    add:#7 絶対importの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
   },
-  plugins: [react()]
-})
+  resolve: {
+    alias: [{ find: "@", replacement: "src" }],
+  },
+  plugins: [react()],
+});
