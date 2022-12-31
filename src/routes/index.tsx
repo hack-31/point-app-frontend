4ef8773bb034de4804ba5ca8fb6ca8b73daee55d
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2023-01-01 06:30:14 +0900

    add:#11 setting router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,13 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
+import { useRoutes } from "react-router-dom";
+import { publicRoutes } from "./public";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { lazyImport } from "@/utils/lazyImport";
-import { Layout } from "@/components/Layout";
-
-const { Users } = lazyImport(() => import("@/pages/Users"), "Users");
-const { Login } = lazyImport(() => import("@/pages/Login"), "Login");
-const { Signup } = lazyImport(() => import("@/pages/Signup"), "Signup");
-
-const theme = createTheme({});
-
+/**
+ * 全体のルーティングの設定
+ */
 export const AppRoutes = () => {
   // TODO: メールアドレス・パスワード変更画面、名前変更画面のルーティング追加
-  return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Users />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              {/*<Route path="/password-reset" element={<PasswordReset />} />
-              <Route path="/account" element={<Account />} /> */}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Box>
-    </ThemeProvider>
-  );
+  const commonRoutes = [{ path: "/", element: <div /> }];
+  const routes = publicRoutes;
+  const element = useRoutes([...routes, ...commonRoutes]);
+  return <>{element}</>;
 };
