33e0718ffd48bc16137b66a874a5af001af2c7c3
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2023-01-01 05:13:30 +0900

    update:#11 providerの修正

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,10 +1,14 @@
+import { AuthProvider } from "@/lib/auth";
 import { queryClient } from "@/lib/react-query";
+import { Box, Button, createTheme, ThemeProvider } from "@mui/material";
 import {
   QueryClientProvider,
   QueryErrorResetBoundary,
 } from "@tanstack/react-query";
 import * as React from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 /**
  * エラー画面
@@ -19,11 +23,20 @@ const ErrorFallback = ({
   error: Error;
 }) => {
   return (
-    <div role="alert">
+    <Box
+      role="alert"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      flexDirection="column"
+    >
       <h2>エラーが発生しました。</h2>
       <p>{error.message}</p>
-      <button onClick={() => reset()}>再取得</button>
-    </div>
+      <Button variant="contained" onClick={() => reset()}>
+        再取得
+      </Button>
+    </Box>
   );
 };
 
@@ -35,22 +48,29 @@ type AppProviderProps = {
  * プロパイダー
  */
 export const AppProvider = ({ children }: AppProviderProps) => {
+  const theme = createTheme({});
   return (
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary
-          onReset={reset}
-          fallbackRender={({ resetErrorBoundary, error }) => (
-            <ErrorFallback reset={resetErrorBoundary} error={error} />
-          )}
-        >
-          <React.Suspense fallback={<div>loading...</div>}>
-            <QueryClientProvider client={queryClient}>
-              {children}
-            </QueryClientProvider>
-          </React.Suspense>
-        </ErrorBoundary>
-      )}
-    </QueryErrorResetBoundary>
+    <RecoilRoot>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            onReset={reset}
+            FallbackComponent={({ error, resetErrorBoundary }) => (
+              <ErrorFallback reset={resetErrorBoundary} error={error} />
+            )}
+          >
+            <React.Suspense fallback={<div>loading...</div>}>
+              <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                  <Router>
+                    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+                  </Router>
+                </AuthProvider>
+              </QueryClientProvider>
+            </React.Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </RecoilRoot>
   );
 };
