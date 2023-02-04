acefc9e96551d54d76a95b186435d85cf3099233
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2023-02-04 18:07:54 +0900

    add:#30 リセットパスワードページを作成

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export * from "./api/register";
 export * from "./api/temporaryRegister";
+export * from "./component/passwordReset";
 export * from "./component/signup";
---
+++ src/pages/PasswordReset/PasswordReset.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+import { PasswordReset } from "@/features/auth";
+
+/**
+ * path: /password-reset
+ */
+export const PasswordResetPage = () => {
+  return (
+    <Box mx="10px">
+      <PasswordReset />
+    </Box>
+  );
+};
