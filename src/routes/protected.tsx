cc671ca9e1035ed22431415addd556b4da327cc1
Yuki-TU <k.yuki.90219@icloud.com>
2023-11-26 04:30:11 +0900

    add:#53 エラーの出し分け * 401の場合は、ログイン画面へ * ログインの場合のみ/userに遷移

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,17 +1,21 @@
 import { CircularProgress } from "@mui/material";
 import { Box } from "@mui/system";
+import { AxiosError } from "axios";
 import { Suspense } from "react";
 import { Outlet, RouteObject } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 import { Error } from "@/components/Error";
 import { MainLayout } from "@/components/Layout";
 import { useAuth } from "@/lib/auth";
 import { AccountPage } from "@/pages/Account";
 import { AccountUpdatePage } from "@/pages/AccountUpdate";
+import { LoginPage } from "@/pages/Login";
 import { NotificationPage } from "@/pages/Notification";
 import { NotificationsPage } from "@/pages/Notifications";
 import { PasswordUpdatePage } from "@/pages/PasswordUpdate";
 import { UsersPage } from "@/pages/Users";
+import storage from "@/utils/storage";
 
 const App = () => {
   return (
@@ -34,6 +38,35 @@ const App = () => {
   );
 };
 
+// エラー時の画面表示
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+  // apiで認証エラーが起きた時
+  if ((error as AxiosError).response?.status === 401) {
+    // useAuthのuser stateが残るためリロードし消す
+    location.reload();
+    // リロードはすぐにされないので、ローディングを表示する
+    return (
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+  // useAuthのuser stateがない時
+  // ログインしていない状態で保護されたページにアクセスした時
+  if ((error as Error).message === "401") {
+    storage.clearToken();
+    return <LoginPage />;
+  }
+  // それ以外はエラー
+  return <Error />;
+};
+
 /**
  * ログインしないと見れないページ
  */
@@ -45,7 +78,7 @@ export const getProtectedRoutes = (): RouteObject[] => {
       element: <App />,
       children: [
         {
-          errorElement: <Error />,
+          errorElement: <ErrorBoundary />,
           loader: async () => {
             if (!user) {
               throw window.Error("401", {
