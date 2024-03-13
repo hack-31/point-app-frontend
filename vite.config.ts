87b9d317dc282aade76207fa44d0d69c25d253fb
Yuki-TU <k.yuki.90219@icloud.com>
2024-03-14 01:44:48 +0900

    add:#57 set vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,40 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: `${process.cwd()}/src` }],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "360度評価アプリ",
+        short_name: "Point App",
+        description: "ポイント送付ができるアプリです。",
+        icons: [
+          {
+            src: "/icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/icon-310.png",
+            sizes: "310x310",
+            type: "image/png",
+          },
+          {
+            src: "/icon-310.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        lang: "ja",
+      },
+    }),
+  ],
 });
